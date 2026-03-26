6ea51a9109178c42972dd30cece8a29659403b63
Elton Eduardo Silva Tosta <75891@senacminas.edu.br>
2026-03-26 08:49:00 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-function verificarnumero() {
-    let numero= document.getElementById("numero").value;
-    let resultado = document.getElementById("resultado");
+function verificarmassacorporal() {
+    let peso= document.getElementById("altura").value;
+    let altura= document.getElementById("peso").value;
 
-    numero = Number(numero);
 
-    if (numero >0) { 
-        resultado.textContent = " O número é positivo";
-        resultado.className = "resultado positivo";
+    let imc = peso / (altura * altura);
+
+
+    if (imc >18.5) { 
+        resultado.textContent = " O número é abaixo do peso";
+        resultado.className = "abaixo do peso";
     }
 
-    else if (numero <0) {
-        resultado.textContent = " O número é negativo";
-        resultado.className = "resultado negativo";
+    else if (imc >= 18.5 && imc < 25) {
+        resultado.textContent = " O número é peso normal";
+        resultado.className = "normal";
     }
 
-    else { 
-          resultado.textContent = "O número é zero"
-          resultado.className = " resultado é zero"
+    else if (imc >= 25 && imc < 30) {
+        resultado.textContent = " O número é sobrepeso";
+        resultado.className = "sobrepeso";
+    }
+
+    else if  (imc >30) { 
+          resultado.textContent = "o número obesidade"
+          resultado.className = " obesidade"
     }
 }
